2741ab22dac216971c2ddd37502ba7a95dfe4659
Eduardo Bart <edub4rt@gmail.com>
2024-05-08 07:51:00 -0300

    Fix iframe loading

--- src/pages/playground.tsx
+++ src/pages/playground.tsx
@@ -21,16 +21,16 @@ void main() { // entry point
     riv_draw_rect_fill(0, 200, 256, 56, RIV_COLOR_PEACH); // draw beach
     riv_draw_circle_fill(x, y, 8, RIV_COLOR_RED); // draw red dot
     if (y >= 202) { // check if red dot reached the beach
-      riv_draw_text("MAX IS AT THE BEACH\nYOU SAVED HIM\nHE IS VERY HAPPY", RIV_SPRITESHEET_FONT_5X7,
+      riv_draw_text("MAX IS AT THE BEACH\\nYOU SAVED HIM\\nHE IS VERY HAPPY", RIV_SPRITESHEET_FONT_5X7,
                     RIV_CENTER, 128, 128, 2, RIV_COLOR_YELLOW); // draw end screen
       riv->quit = true; // end game
     } else if (y <= 32) {
       riv_clear(RIV_COLOR_BLACK); // clear screen
-      riv_draw_text("MAX WAS LOST AT\nTHE DEEP OCEAN\nHIS FATE IS UKNOWN", RIV_SPRITESHEET_FONT_5X7,
+      riv_draw_text("MAX WAS LOST AT\\nTHE DEEP OCEAN\\nHIS FATE IS UNKNOWN", RIV_SPRITESHEET_FONT_5X7,
                     RIV_CENTER, 128, 128, 2, RIV_COLOR_RED); // draw text
       riv->quit = true; // end game
     } else {
-      riv_draw_text("THE RED DOT IS MAX\nHE IS LOST AT SEA\nHELP HIM REACH THE BEACH", RIV_SPRITESHEET_FONT_5X7,
+      riv_draw_text("THE RED DOT IS MAX\\nHE IS LOST AT SEA\\nHELP HIM REACH THE BEACH", RIV_SPRITESHEET_FONT_5X7,
                     RIV_CENTER, 128, 64, 1, RIV_COLOR_WHITE); // draw text
     }
   } while(riv_present()); // refresh screen and wait next frame
@@ -48,7 +48,7 @@ void main() { // entry point
               <div className="row row--no-gutters">
                 <div className="col col--6">
                   <div align="center">
-                    <iframe id="rivemu" src="https://emulator.rives.io/#simple=true&editor=true" allowFullScreen className="rivemu-frame" />
+                    <iframe id="rivemu" allowFullScreen className="rivemu-frame" />
                   </div>
                 </div>
                 <div className="col col--6">
@@ -59,9 +59,18 @@ void main() { // entry point
                       onChange={onChange}
                       onCreateEditor={() => {
                         let rivemuFrame = document.getElementById('rivemu');
-                        rivemuFrame.addEventListener("load", function() {
-                          rivemuFrame.contentWindow.postMessage({code:value}, "*");
-                        });
+                        let rivemuFrameWindow = rivemuFrame.contentWindow;
+                        window.addEventListener(
+                          'message',
+                          (e) => {
+                            let params = e.data;
+                            if (params.rivemuLoaded) {
+                                rivemuFrameWindow.postMessage({rivemuRunCode: true, code:value}, "*");
+                            }
+                          },
+                          false,
+                        );
+                        rivemuFrame.src = "https://emulator.rives.io/#simple=true&editor=true";
                       }}
                       extensions={[
                           cpp({}),
@@ -69,14 +78,14 @@ void main() { // entry point
                             {
                               key: 'Shift-Enter',
                               run: () => {
-                                document.getElementById("rivemu").contentWindow.postMessage({code:value, start:true}, "*");
+                                document.getElementById("rivemu").contentWindow.postMessage({rivemuRunCode: true, code:value, start:true}, "*");
                                 return true;
                               },
                             },
                             {
                               key: 'Ctrl-Shift-Enter',
                               run: () => {
-                                document.getElementById("rivemu").contentWindow.postMessage({code:value, start:true}, "*");
+                                document.getElementById("rivemu").contentWindow.postMessage({rivemuRunCode: true, code:value, start:true}, "*");
                                 document.getElementById("rivemu").contentWindow.focus();
                                 return true;
                               },
