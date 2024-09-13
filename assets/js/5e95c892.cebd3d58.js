523147aa21089aa34dd9a0b15437ad365845aae9
Carlo Fragni <carlo@prototyp3.dev>
2024-09-13 11:12:19 -0300

    Deploy website - based on b31711091269d61bf7691c3f47ed0a4a7c046ac1

--- assets/js/5e95c892.cebd3d58.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkrives_docs=self.webpackChunkrives_docs||[]).push([[9647],{7121:(e,s,r)=>{r.r(s),r.d(s,{default:()=>i});r(6540);var c=r(4164),u=r(5500),a=r(7559),d=r(2831),n=r(9201),t=r(4848);function i(e){return(0,t.jsx)(u.e3,{className:(0,c.A)(a.G.wrapper.docsPages),children:(0,t.jsx)(n.A,{children:(0,d.v)(e.route.routes)})})}}}]);
